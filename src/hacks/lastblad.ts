9dca73f3fdcfcf4a0609c3dd82194d3bd67149f8
Matt Greer <matt.e.greer@gmail.com>
2024-06-26 17:39:33 -0400

    lastblad good to go

--- src/hacks/lastblad.ts
+++ src/hacks/lastblad.ts
@@ -82,12 +82,13 @@ const lastblad: RomHackGameEntry = {
       id: "minigame",
       creators: ["Matt Greer"],
       name: "Hidden mini game",
-      zip: "",
+      zip: "lastbladminigameIpsPatches.zip",
       repo: "https://github.com/city41/lbmini",
       incompatibleWith: [],
       details: [
         "The developers of the game hid a mini game deep inside of it. It's normally tricky to get to.",
         "This hack has the mini game play instead of the attract mode.",
+        "In both AES/Home and MVS/arcade mode, the mini game will start right after the Neo Geo eyecatcher has finished (don't try to skip it).",
       ],
       screenshots: [
         {
