729b3aa2da99eabcb061ef14b1074a45a5f6555f
Matt Greer <matt.e.greer@gmail.com>
2025-10-06 22:43:37 -0400

    patch started

--- src/hacks/index.ts
+++ src/hacks/index.ts
@@ -3,5 +3,12 @@ import { kof94 } from "./kof94";
 import { pbobblen } from "./pbobblen";
 import { lastblad } from "./lastblad";
 import { samsho4 } from "./samsho4";
+import { maglord } from "./maglord";
 
-export const games: RomHackGameEntry[] = [kof94, pbobblen, lastblad, samsho4];
+export const games: RomHackGameEntry[] = [
+  kof94,
+  pbobblen,
+  lastblad,
+  samsho4,
+  maglord,
+];
---
+++ src/hacks/maglord.ts
@@ -0,0 +1,101 @@
+import { RomHackGameEntry } from "@/types";
+import { Genre } from "neosdconv/lib/genres";
+
+const maglord: RomHackGameEntry = {
+  gameName: "Magician Lord",
+  mameName: "maglord",
+  developer: "ADK",
+  year: 1990,
+  neosdConvertOptions: {
+    genre: Genre.Action,
+    ngh: "005",
+  },
+  originalFiles: [
+    {
+      fileName: "005-c1.c1",
+      size: 524288,
+      sha: "98a7b51b6ad5e2556e57c5de27fe17f18e175fc59a9497743b8c8aeba969ee35",
+    },
+    {
+      fileName: "005-c2.c2",
+      size: 524288,
+      sha: "e2249dddc5d90dcf4a1c526ff8723748d5119f7c8970e417eb7113260aff1acf",
+    },
+    {
+      fileName: "005-c3.c3",
+      size: 524288,
+      sha: "d31de38ea72198fd6c8c16b41394281fa2fea8efa8a732a25438495cd8220f90",
+    },
+    {
+      fileName: "005-c4.c4",
+      size: 524288,
+      sha: "b1a3ca5b61802e57897432c4bce10ff8e77863124b5c91d982d9d2da1823fe5a",
+    },
+    {
+      fileName: "005-c5.c5",
+      size: 524288,
+      sha: "3a2b1c907c543b5bc70e6a069fcb5c1505c547b0962673c2d43409cddd2fbfb0",
+    },
+    {
+      fileName: "005-c6.c6",
+      size: 524288,
+      sha: "5423f16077c806c91ff7555ae849e06c126c22b164344082f1d9ccd2a3040bea",
+    },
+    {
+      fileName: "005-m1.m1",
+      size: 262144,
+      sha: "99b4ccd4d84f105905ed69d005c1bdebc2eba4cded2cc2daff43c0bdb97c8f0e",
+    },
+    {
+      fileName: "005-p1.p1",
+      size: 524288,
+      sha: "a315c99a9bca39fd0b37e2313717013697d2534b6f7f524cf398cc27e4278915",
+    },
+    {
+      fileName: "005-s1.s1",
+      size: 131072,
+      sha: "a7124fdb61bc9603884507cc1e92cf58de35f67c7226ff46125f511ab3393f5f",
+    },
+    {
+      fileName: "005-v1.v1",
+      size: 4194304,
+      sha: "e2288fc82288711258d87aa605a2efb1ca6563d324d7f59bb7f4b312a3fb5938",
+    },
+    {
+      fileName: "005-v2.v2",
+      size: 4194304,
+      sha: "27600fe09f1bf12dc0e9c55eb650f2aaa514739a02d7b0373f6a76d277ae4f51",
+    },
+    {
+      fileName: "005-v3.v3",
+      size: 4194304,
+      sha: "b0296bf7d8f62473c380a751d86ac690c3e4c53a9b881b97b5cfe2252255d959",
+    },
+  ],
+  hacks: [
+    {
+      id: "acfh",
+      creators: ["Boomchild"],
+      name: "Again Changes From Hell",
+      zip: "maglord_acfh_1.0.0.zip",
+      repo: "https://github.com/Boomchil/maglord-acfh",
+      incompatibleWith: [],
+      details: [
+        "The developers of the game hid a mini game deep inside of it. It's normally tricky to get to.",
+        "This hack has the mini game play instead of the attract mode.",
+        "The only purpose of this hack is to expose the minigame. When done checking it out, please go back to a regular Last Blade ROM for playing the game.",
+        "In both AES/Home and MVS/arcade mode, the mini game will start right after the Neo Geo eyecatcher has finished (don't try to skip it).",
+        "Controls: B to kick a lifesaver, D to restart the level.",
+      ],
+      screenshots: [
+        {
+          fileName: "ss1.png",
+          description: "The first level",
+        },
+      ],
+      downloadAs: ["mame", "neosd"],
+    },
+  ],
+};
+
+export { maglord };
